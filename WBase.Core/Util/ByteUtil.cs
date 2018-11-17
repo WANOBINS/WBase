65ee0a22f6ac27b1da0eadad6641787e38628071
DaemonUmbra <daemon.umbra@gmail.com>
2018-11-17 15:03:30 -0500

    Added Byte Utilities

---
+++ WBase.Core/Util/ByteUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WBase.Core.Util
+{
+    public static class ByteUtil
+    {
+        private static Dictionary<Type, int> Cache = new Dictionary<Type, int>();
+
+        public static int BytesIn(Type t)
+        {
+            if (!Cache.ContainsKey(t))
+            {
+#pragma warning disable CS0618 // Type or member is obsolete
+                Cache.Add(t, System.Runtime.InteropServices.Marshal.SizeOf(t) / 8);
+#pragma warning restore CS0618 // Type or member is obsolete
+            }
+            return Cache[t];
+        }
+    }
+}
