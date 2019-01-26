f0d77c1c3d7873f2a5af3fa2906b7fa84cb5811f
DaemonUmbra <daemon.umbra@gmail.com>
2019-01-26 15:20:05 -0500

    Added GameObjectArrayExtensions

---
+++ WBase.Unity/Extensions/GameObjectArrayExt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WBase.Unity.Extensions
+{
+    public static class GameObjectArrayExt
+    {
+        public static GameObject GetClosestTo(this GameObject[] GameObjects, Vector3 Position)
+        {
+            GameObject Closest = null;
+            foreach(GameObject go in GameObjects)
+            {
+                if(Closest == null || (go.transform.position - Position).magnitude < (Closest.transform.position - Position).magnitude)
+                {
+                    Closest = go;
+                }
+            }
+            return Closest;
+        }
+    }
+}
