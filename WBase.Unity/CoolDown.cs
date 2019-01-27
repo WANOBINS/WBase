6314c1c8ca199566c9094f30c078ed75158c54a1
DaemonUmbra <daemon.umbra@gmail.com>
2019-01-27 12:40:08 -0500

    Added Cooldown

---
+++ WBase.Unity/CoolDown.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WBase.Unity
+{
+    public class CoolDown
+    {
+        private float? LastActivation = null;
+        public float Delay;
+
+        public CoolDown(float Delay)
+        {
+            this.Delay = Delay;
+        }
+
+        public bool Start()
+        {
+            if(LastActivation == null || Time.time >= LastActivation + Delay)
+            {
+                LastActivation = Time.time;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
